b8c89a147bf14450cc433fddc7382dc37149b98c
karthik33399 <karthikselvaraj269@gmail.com>
2026-06-26 23:45:17 +0530

    Add files via upload
    yes

---
+++ Assignment-NumberType.js
@@ -0,0 +1,20 @@
+function checkNumbers(num)
+{
+    if (num>0)
+    {
+
+    console.log("Ths number is positive number");
+    }
+    
+        else if (num<0)
+        {
+ console.log("Ths number is negative number");
+        }
+        else{
+             console.log("Ths number is netrual");
+        }
+        
+    }
+checkNumbers(1)
+checkNumbers(-1)
+checkNumbers(0)
